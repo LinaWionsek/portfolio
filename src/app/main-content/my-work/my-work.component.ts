491d082e7c94fc306afd41d457b18934c79e750e
fshippman <fshippman@yahoo.com>
2024-06-12 19:48:50 +0200

    project structure change

---
+++ src/app/main-content/my-work/my-work.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProjectsComponent } from './projects/projects.component';
+@Component({
+  selector: 'app-my-work',
+  standalone: true,
+  imports: [ProjectsComponent],
+  templateUrl: './my-work.component.html',
+  styleUrl: './my-work.component.scss'
+})
+export class MyWorkComponent {
+
+}
