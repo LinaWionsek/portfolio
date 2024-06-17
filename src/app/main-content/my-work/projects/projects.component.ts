2e96d4fd3a519479bbcfc7a419c8309bb7dda9d4
fshippman <fshippman@yahoo.com>
2024-06-17 15:26:40 +0200

    project section start

--- src/app/main-content/my-work/projects/projects.component.ts
+++ src/app/main-content/my-work/projects/projects.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ProjectsService} from '../../../services/projects/projects.service';
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  projectsservice = inject(ProjectsService);
 
+  openGithub(url: string) {
+    window.open(url, '_blank');
+  }
 }
--- src/app/services/projects/projects.service.ts
+++ src/app/services/projects/projects.service.ts
@@ -9,10 +9,26 @@ export class ProjectsService {
 
   projects = [
     {
-      img: 'angular.png',
+      img: 'join.png',
       name: 'Join',
-      stack: 'Angular',
-      description: 'ggagakkgag',
+      stack: 'Angular | TypeScript | HTML | CSS | Firebase ',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      demo: 'https://github.com',
+      github: 'https://github.com',
+    },
+    {
+      img: 'join.png',
+      name: 'Pollo Loco',
+      stack: 'JavaScript | HTML | CSS',
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      demo: 'https://github.com',
+      github: 'https://github.com',
+    },
+    {
+      img: 'join.png',
+      name: 'Pokedex',
+      stack: 'JavaScript | HTML | CSS',
+      description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
       demo: 'https://github.com',
       github: 'https://github.com',
     },
