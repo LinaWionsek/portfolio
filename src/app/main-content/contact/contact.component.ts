5d2e8a73d65a89d8ea9539d0591e36ced0ff77e3
fshippman <fshippman@yahoo.com>
2024-07-08 15:53:11 +0200

    basic form validation without logic

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
   onChange(event: Event): void {
-  console.log((event.target as HTMLInputElement).checked);
-   }
+    console.log((event.target as HTMLInputElement).checked);
+  }
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log('submitted');
+    }
+  }
 }
