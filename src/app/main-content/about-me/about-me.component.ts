e7fa7e2b558a86b8501e773f0512d85b915d8cc7
fshippman <fshippman@yahoo.com>
2024-07-16 11:15:21 +0200

    button animation changes

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -5,8 +5,10 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 export class AboutMeComponent {
-
+  scrollToContactForm() {
+    window.location.href = '#contact';
+  }
 }
