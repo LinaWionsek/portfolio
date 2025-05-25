e10a635d456650006e6199ffa7d3f8b53f014f6d
Lina Wion <lina@test.com>
2025-05-25 19:14:35 +0200

    changed few german sentences

--- src/app/services/projects/projects.service.ts
+++ src/app/services/projects/projects.service.ts
@@ -23,7 +23,7 @@ export class ProjectsService {
       description:
         'A real-time chat application for team communication, inspired by Slack. Connect with your team in real time, via direct messages or in a channel.',
       description_de:
-        'Eine Echtzeit-Chat-Anwendung für Teamkommunikation, inspiriert von Slack. Tausche dich in Echtzeit mit deinem Team aus: per Direktnachricht oder in einem Channel.',
+        'Eine Echtzeit-Chat-Anwendung für Teamkommunikation, inspiriert von Slack. Tauschen Sie sich in Echtzeit mit Ihrem Team aus: per Direktnachricht oder in einem Channel.',
       demo: 'https://dabubble.lina-wionsek.de/',
       github: 'https://github.com/LinaWionsek/dabubble',
     },
@@ -34,7 +34,7 @@ export class ProjectsService {
       description:
         'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
       description_de:
-        'Aufgabenmanager, inspiriert vom Kanban-System. Erstelle und organisiere Aufgaben per Drag-and-Drop, weise Benutzer und Kategorien zu.',
+        'Aufgabenmanager, inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben per Drag-and-Drop, weisen Sie Benutzer und Kategorien zu.',
       demo: 'https://join.lina-wionsek.de/',
       github: 'https://github.com/LinaWionsek/join',
     },
@@ -45,7 +45,7 @@ export class ProjectsService {
       description:
         'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
       description_de:
-        'Spring-, Renn- und Wurfspiel basierend auf objektorientiertem Ansatz. Hilf Pepe dabei, Münzen und Tabasco-Sauce zu finden, um gegen die verrückte Henne zu kämpfen.',
+        'Spring-, Renn- und Wurfspiel basierend auf objektorientiertem Ansatz. Helfen Sie Pepe dabei, Münzen und Tabasco-Sauce zu finden, um gegen die verrückte Henne zu kämpfen.',
       demo: 'http://polloloco.lina-wionsek.de/',
       github: 'https://github.com/LinaWionsek/el-pollo-loco',
     },
